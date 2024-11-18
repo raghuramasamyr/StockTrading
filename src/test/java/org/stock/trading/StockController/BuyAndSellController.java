6347b1f45a9fc456dcbae0c2a737d1b24f684d34
Raghu <raghuramasamyr@gmail.com>
2024-11-18 19:28:50 +0530

    STOCK TRADING

--- src/test/java/org/stock/trading/StockController/BuyAndSellController.java
+++ src/test/java/org/stock/trading/StockController/BuyAndSellController.java
@@ -1,7 +1,13 @@
 package org.stock.trading.StockController;
 
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class BuyAndSellController {
 
+    @Test
     void test(){
         assert (true);
     }
